0be768d4161d923e6ff44b0e3393ace8b1d26592
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-06-16 14:46:01 +0530

    BorrowRecord Entity

---
+++ src/main/java/com/librarysystem/Entities/BorrowRecord.java
@@ -0,0 +1,36 @@
+package com.librarysystem.Entities;
+
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "borrow_records")
+public class BorrowRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column(nullable = false)
+    private LocalDate borrowDate;
+
+    @Column
+    private LocalDate returnDate;
+}
